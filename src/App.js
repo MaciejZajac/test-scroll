22a2a7c0704148cb470800c926dcc32318d09f01
mzajac <mzajac@corelogic.pl>
2021-04-14 20:12:32 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import Picker from 'rmc-picker';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [state, setState] = React.useState([1]);
+    const [items, setItems] = React.useState([]);
+
+    const onChange = (value) => {
+        setState(value);
+    };
+
+    // const onScrollChange = (value) => {
+    //     console.log('onScrollChange', value);
+    // };
+
+    const getItems = () => {
+        const items = [];
+        for (let i = 0; i < 3000; i++) {
+            items.push(
+                <Picker.Item value={i + ''} key={i}>
+                    {'A lot stuff going on!' + i}
+                </Picker.Item>
+            );
+        }
+        return items;
+    };
+
+    useEffect(() => {
+        const newItems = getItems();
+
+        setItems(newItems);
+    }, []);
+
+    return (
+        <div>
+            <div style={{ background: '#f5f5f9', padding: 10 }}>
+                <Picker selectedValue={state} onValueChange={onChange}>
+                    {items}
+                </Picker>
+            </div>
+        </div>
+    );
 }
 
 export default App;
